00a2efb6589f9641c035752cd09a856bf4e38ae0
Silver Dragon <silverdragon013@hotmail.com>
2020-06-17 08:58:37 -0400

    Added unit structure and started gitignore

---
+++ WOKArmyBuilder/WOKArmyBuilder/WOKArmyBuilder/Models/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WOKArmyBuilder.Models
+{
+    public class Unit
+    {
+        // Basic info
+        public string Name { get; set; }
+        public string Faction { get; set; }
+        public string Trait { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public bool Character { get; set; }
+        public char DeploymentZone { get; set; }
+
+        // Size info
+        public int Base { get; set; }
+        public int Size { get; set; }
+    }
+}
