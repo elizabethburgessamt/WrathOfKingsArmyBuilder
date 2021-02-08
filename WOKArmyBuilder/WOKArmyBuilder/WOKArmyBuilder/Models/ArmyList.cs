53a2fd8b5f69dabc89c0d8a496b94212f9a66e1d
Silver Dragon <silverdragon013@hotmail.com>
2021-02-08 09:57:15 -0500

    Added models from #14

---
+++ WOKArmyBuilder/WOKArmyBuilder/WOKArmyBuilder/Models/ArmyList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WOKArmyBuilder.Models
+{
+    public class ArmyList
+    {
+        public string Name { get; set; }
+        public string Faction { get; set; }
+        public int ArmyPoints { get; set; }
+        public string ArmySize { get; set; }
+        public List<ArmyListItem> ArmyListItems { get; set; }
+    }
+}
---
+++ WOKArmyBuilder/WOKArmyBuilder/WOKArmyBuilder/Models/ArmyListItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WOKArmyBuilder.Models
+{
+    public class ArmyListItem
+    {
+        public Unit unit { get; set; }
+        public int Quantity { get; set; }
+    }
+}
