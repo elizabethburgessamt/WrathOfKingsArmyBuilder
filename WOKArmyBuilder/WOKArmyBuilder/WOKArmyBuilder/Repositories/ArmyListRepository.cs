de65663b8bec0505a5a30c214c206bb19c24e661
Silver Dragon <silverdragon013@hotmail.com>
2021-02-18 13:15:04 -0500

    Added repos interface and class related to #16

---
+++ WOKArmyBuilder/WOKArmyBuilder/WOKArmyBuilder/Repositories/ArmyListRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WOKArmyBuilder.Models;
+using SQLite;
+using System.IO;
+
+namespace WOKArmyBuilder.Repositories
+{
+    public class ArmyListRepository : IArmyListRepository
+    {
+        private SQLiteAsyncConnection connection;
+
+        public event EventHandler<ArmyList> OnListAdded;
+        public event EventHandler<ArmyList> OnListUpdated;
+        public event EventHandler<ArmyList> OnListDeleted;
+
+        public Task DeleteList(ArmyList list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ArmyList>> GetArmyLists()
+        {
+            await CreateConnection();
+            return await connection.Table<ArmyList>().ToListAsync();
+        }
+
+        public async Task<List<Unit>> GetUnits()
+        {
+            await CreateConnection();
+            return await connection.Table<Unit>().ToListAsync();
+        }
+
+        public Task NewList(ArmyList list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateList(ArmyList list)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task CreateConnection()
+        {
+            if (connection != null)
+            {
+                return;
+            }
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var databasePath = Path.Combine(documentPath, "WOKArmyLists.db");
+
+            connection = new SQLiteAsyncConnection(databasePath);
+            await connection.CreateTableAsync<ArmyList>();
+
+            if (await connection.Table<ArmyList>().CountAsync() == 0)
+            {
+                await connection.InsertAsync(new ArmyList()
+                {
+                    Name = "New Army List",
+                    Faction = "Nassier",
+                    ArmyPoints = 16,
+                    ArmySize = "Intro"
+                });
+            }
+
+            // must find a way to populate this
+            await connection.CreateTableAsync<Unit>();
+        }
+    }
+}
---
+++ WOKArmyBuilder/WOKArmyBuilder/WOKArmyBuilder/Repositories/IArmyListRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WOKArmyBuilder.Models;
+
+namespace WOKArmyBuilder.Repositories
+{
+    public interface IArmyListRepository
+    {
+        event EventHandler<ArmyList> OnListAdded;
+        event EventHandler<ArmyList> OnListUpdated;
+        event EventHandler<ArmyList> OnListDeleted;
+
+        Task<List<ArmyList>> GetArmyLists();
+        Task NewList(ArmyList list);
+        Task UpdateList(ArmyList list);
+        Task DeleteList(ArmyList list);
+
+        Task<List<Unit>> GetUnits();
+    }
+}
